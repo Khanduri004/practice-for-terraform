04b7f13c142be9e4943f7200605f3899a1a4cfb3
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-09-09 20:41:30 +0100

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,17 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  for_each = toset(["one", "two", "three"])
+
+  name = "instance-${each.key}"
+
+  instance_type = "t3.micro"
+  key_name      = "user1"
+  monitoring    = true
+  subnet_id     = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
