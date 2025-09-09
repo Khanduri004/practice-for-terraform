782c6178b6f45d7ddfea6759c0de314816b95567
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-09-09 23:17:24 +0100

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,8 @@
+resource "aws_kms_key" "Sept_Key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
 resource "aws_s3_bucket" "Sept_bucket" {
   bucket = "Sept_bucket"
 
@@ -5,4 +10,21 @@ resource "aws_s3_bucket" "Sept_bucket" {
     Name        = "Sept_bucket"
     Environment = "Dev"
   }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "Sept_Month" {
+  bucket = aws_s3_bucket.Sept_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.Sept_Key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+
+ resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.Sept_bucket_id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 }
