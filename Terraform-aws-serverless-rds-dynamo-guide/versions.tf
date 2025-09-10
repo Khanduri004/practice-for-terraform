c22272742fa2d9cbe6e1ea01aa93310e027e66a4
Shreya Khanduri <131514715+Khanduri004@users.noreply.github.com>
2025-09-10 20:17:42 +0100

    Create versions.tf

---
+++ Terraform-aws-serverless-rds-dynamo-guide/versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+
+  required_providers {
+    aws = {
+     source = "hashicorp/aws"
+     version = "~> 5.30"
+    }
+   random = {
+     source = "hashicorp/random"
+     version = "~> 3.5"
+    }
+   archive = {
+     source = "hashicorp/archive"
+     version = "~> 2.3"
+    }
+  }
+}
